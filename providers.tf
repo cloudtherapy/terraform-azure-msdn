0f9ba5be25975ab36c69f88666807b4fe310ae50
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-18 18:49:18 -0500

    Updated providers

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "4.14.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "3.0.2"
     }
   }
 }
